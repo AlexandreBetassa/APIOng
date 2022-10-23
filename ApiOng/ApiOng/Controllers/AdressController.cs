1ebf8d4ce8328c75ea5a4d74c7b05a02168cad3c
Alexandre Betassa <alexandre.betassa.souza@hotmail.com>
2022-10-23 17:24:26 -0300

    consumido api via cep

---
+++ ApiOng/ApiOng/Controllers/AdressController.cs
@@ -0,0 +1,27 @@
+﻿using ApiOng.Models;
+using ApiOng.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+
+namespace ApiOng.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdressController : ControllerBase
+    {
+        private readonly AdressRepository _adressRepository;
+        public AdressController() 
+        {
+            _adressRepository = new AdressRepository();
+        }
+
+    [HttpGet("{cep}")]
+        public ActionResult<Adress> GetAdress(string cep)
+        {
+            var adress = _adressRepository.GetAdress(cep).Result;
+            if (adress == null) return NotFound();
+            return Ok(adress);
+        }
+    }
+}
---
+++ ApiOng/ApiOng/Repositories/AdressRepository.cs
@@ -0,0 +1,29 @@
+﻿using ApiOng.Models;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Net.Http.Headers;
+using System.Net;
+using MongoDB.Bson;
+using System;
+
+namespace ApiOng.Repositories
+{
+    public class AdressRepository
+    {
+        public AdressRepository() { }
+        //{
+        //    _adress = new HttpClient();
+
+        //}
+
+        public async Task<String> GetAdress(string cep)
+        {
+            using (HttpClient _adressClient = new HttpClient())
+            {
+                HttpResponseMessage response = await _adressClient.GetAsync("https://viacep.com.br/ws/" + cep + "/json/");
+                var adress = await response.Content.ReadAsStringAsync();
+                return adress;
+            }
+        }
+    }
+}
